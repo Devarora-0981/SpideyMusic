02e2a668fe54d229c477546784007506db9f2f3c
Dev Arora <105285580+Devarora0987@users.noreply.github.com>
2022-10-01 00:39:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ get_queue = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 
-ASS_HANDLER = list(getenv("ASS_HANDLER", ".").split())
+ASS_HANDLER = list(getenv("ASS_HANDLER", "!").split())
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
@@ -24,4 +24,4 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/FallenXBots")
 
 STRING_SESSION = getenv("STRING_SESSION", None)
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5738896728").split()))
