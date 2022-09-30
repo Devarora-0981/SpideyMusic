378ae41d5860cf1e43efa989b1a188c936d34f27
Dev Arora <105285580+Devarora0987@users.noreply.github.com>
2022-10-01 02:07:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ get_queue = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 
-ASS_HANDLER = list(getenv("ASS_HANDLER")
+ASS_HANDLER = list(getenv("ASS_HANDLER", ".").split())
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
