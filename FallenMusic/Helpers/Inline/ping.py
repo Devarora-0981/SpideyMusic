eeacb101261e035a3fd7a76612b8d3dff2e58169
NeonTFB <116315924+NeonTFB@users.noreply.github.com>
2022-10-22 01:58:20 +0530

    Update ping.py

--- FallenMusic/Helpers/Inline/ping.py
+++ FallenMusic/Helpers/Inline/ping.py
@@ -3,16 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 ping_ig = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text="sᴜᴩᴩᴏʀᴛ",
-                    url=config.SUPPORT_CHAT,
-                ),
-                InlineKeyboardButton(
-                    text="sᴏᴜʀᴄᴇ",
-                    url="https://te.legra.ph/file/c3a785291f07a6f580ac3.mp4"
-                )
-            ]
-        ]
-    )
+    [
+        [InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")],
+    ]
+)
