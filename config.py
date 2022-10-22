92911986cdb60981f4b00e9984fe366ba39d09d4
Dev Arora <115332419+Devarora-0981@users.noreply.github.com>
2022-10-23 01:28:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ get_queue = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 
-ASS_HANDLER = list(getenv("ASS_HANDLER", "/").split())
+ASS_HANDLER = list(getenv("ASS_HANDLER", "/", ".").split())
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 LOGGER_ID = int(getenv("LOGGER_ID"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5350929381").split()))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
 START_IMG = getenv("START_IMG")
@@ -25,4 +25,4 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/Devbotz")
 
 STRING_SESSION = getenv("STRING_SESSION", None)
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5738896728").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5350929381").split()))
